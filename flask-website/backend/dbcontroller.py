8642624c641ca17cddc3b5f240560a2e6e4596e7
Timofey Martynov <mart-tima@yandex.ru>
2019-11-14 20:35:17 +0500

    add query func for insert in db + select tables

--- flask-website/backend/dbcontroller.py
+++ flask-website/backend/dbcontroller.py
@@ -1,24 +1,38 @@
 from sqlalchemy import create_engine
-import psycopg2
 
 engine = create_engine('postgresql+psycopg2://postgres:root@localhost/pansionat')
-result = engine.execute("SELECT * FROM corps;")
+
+
+# result = engine.execute("SELECT * FROM corps;")
+
+def show_table(name_table):
+    result = engine.execute("SELECT * FROM %s;" % name_table)
+    for row in result:
+        print(row)
+    return result
 
 
 def add_room(id_status, id_type, id_corp):
-    pass
+    result = engine.execute("INSERT INTO rooms(id_status, id_type, id_corp)"
+                            " VALUES ('%s', '%s', '%s');" % (id_status, id_type, id_corp))
+    return "Success"
 
 
 def add_corp(name_corp):
-    pass
+    result = engine.execute("INSERT INTO corps(name_corp) VALUES ('%s');" % name_corp)
+    return "Success"
 
 
 def add_type_rooms(name_type, place_amount, price):
-    pass
+    result = engine.execute("INSERT INTO type_rooms(name_type, place_amount, price)"
+                            " VALUES ('%s', '%s', '%s');" % (name_type, place_amount, price))
+    return "Success"
 
 
 def add_status(name_status):
-    pass
+    result = engine.execute("INSERT INTO statuses(name_status)"
+                            " VALUES ('%s');" % name_status)
+    return "Success"
 
 
 def add_request(id_room, date_start, date_end, id_status):
--- flask-website/backend/main.py
+++ flask-website/backend/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import dbcontroller
+from flask import Flask, render_template, abort, redirect, url_for, session
+import dbcontroller as dbc
 import os
 
 template_dir = os.path.abspath('../templates/')
@@ -11,16 +11,30 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/list')
-def list():
+@app.route('/list/')
+def list_rooms():
+    abort(401)
     return render_template('listRooms.html')
 
 
-@app.route('/request')
+@app.route('/request/')
 def request():
     return render_template('requestTicket.html')
 
 
-@app.route('/journal')
+@app.route('/journal/')
 def journal():
     return render_template('journal.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Такой страницы нет", 404
+
+
+@app.errorhandler(401)
+def page_login(error):
+    return redirect(url_for('index'))
+
+
+app.secret_key = str(os.urandom(24))
